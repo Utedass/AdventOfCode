8410234758b9571949e4f366aa5d884a39b2b65f
Jonatan Gezelius <Jonatan.Gezelius@syncore.se>
2021-12-01 08:44:21 +0100

    Add skeleton code

---
+++ 01/a.py
@@ -0,0 +1,22 @@
+import os, argparse
+
+
+def solve_task(filename):
+    with open(filename) as infile:
+        for line in infile:
+            pass
+            
+    os.system("pause")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Script that toggles output of a Tenma PSU",epilog="Have a nice day!")
+    parser.add_argument('filename', nargs='?', default="infile.txt", help='Input file')
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arguments()
+    solve_task(args.filename)
+
+if __name__ == "__main__":
+    main()
